8a971a2568be8676c3fcf2a4e48d27ccbbd6413c
João <moreirajoaopaulo951@gmail.com>
2026-08-16 21:56:30 -0300

    fix: corrigindo rota NinjaController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,7 +2,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
