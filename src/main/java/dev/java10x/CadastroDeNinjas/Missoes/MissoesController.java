70008932161513908a426588eb3403e9d8b514ea
João <moreirajoaopaulo951@gmail.com>
2026-08-16 21:55:14 -0300

    fix: corrigindo rotas MissoesController

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,34 +2,33 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
-    @GetMapping("/missoes")
-    public String msm() {
-        return "Essa é minha missao Ninja";
-    }
-
-    @PostMapping("/adicionar")
+    // Post - mandar requisicao para criar
+    @PostMapping("/criar")
     public String adicionarMissao() {
         return "Adicionar Missao";
     }
 
+    //Get - mandar requisicao para pegar
     @GetMapping("/todos")
-    public String mostrarTodasMissoes() {
+    public String listarTodasMissoes() {
         return "Mostrar todas missoes";
     }
 
     @GetMapping("/todosID")
-    public String mostrarMissaoPorID(){
+    public String listarMissaoPorID(){
         return "Mostrar missao por ID";
     }
 
+    // Put - mandar requisicao para alterar
     @PutMapping("/alterarID")
     public String alterarMissaoPorID() {
         return "Alterar por ID";
     }
 
+    // Deletar - mandar requisicao para deletar
     @DeleteMapping("/deletarID")
     public String deletarMissao() {
         return "Deletar Missao";
